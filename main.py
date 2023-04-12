7e2d25f760c34e630212db96087c905b0026200b
Muhammad Farhan <farhan4main@gmail.com>
2023-04-12 19:39:00 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+# TODO: Create a letter using starting_letter.txt
+# for each name in invited_names.txt
+# Replace the [name] placeholder with the actual name.
+# Save the letters in the folder "ReadyToSend".
+
+PLACEHOLDER = "[name]"
+
+# read the 'starting_letter.txt' file
+with open("./Input/Letters/starting_letter.txt") as letter_file:
+    content = letter_file.read()
+
+# read the 'invited_names.txt' file
+with open("./Input/Names/invited_names.txt") as invited_names:
+    # read letter line by line
+    for person in invited_names.readlines():
+        # Remove the new line escape sequence using strip() method
+        stripped_name = person.strip()
+        # write separate file for each person present in 'invited_names.txt' file
+        with open(f"./Output/ReadyToSend/letter_for_{stripped_name}.txt", mode='w') as invitation:
+            # replace the '[name]' with actual name of the person and then write
+            new_letter = content.replace(PLACEHOLDER, stripped_name)
+            invitation.write(new_letter)
+
+# Hint1: This method will help you: h ttps://www.w3schools.com/python/ref_file_readlines.asp
+# Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
+# Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
